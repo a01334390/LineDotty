21c2d19f4587e93244349f4514736cecac6d4bad
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-13 20:36:52 -0600

    Added Line fixing algorithm and changed lines issues

--- LineDotty/LineDotty/ViewController.swift
+++ LineDotty/LineDotty/ViewController.swift
@@ -81,10 +81,59 @@ class ViewController: UIViewController {
         let render = UIGraphicsImageRenderer(bounds: view.bounds)
         renderedLines.image = render.image { ctx in
             for connection in connections {
-                UIColor.green.set()
+                var isLineClear = true
+                for other in connections {
+                    if linesCross(start1: connection.center,
+                                  end1: connection.after.center,
+                                  start2: other.center,
+                                  end2: other.after.center) != nil {
+                        isLineClear = false
+                        break
+                    }
+                }
+                
+                if isLineClear {
+                    UIColor.green.set()
+                } else {
+                    UIColor.red.set()
+                }
+                
                 ctx.cgContext.strokeLineSegments(between: [connection.after.center, connection.center])
             }
         }
+    }
+    
+    func linesCross(start1: CGPoint, end1: CGPoint, start2: CGPoint, end2: CGPoint) -> (x: CGFloat, y: CGFloat)? {
+        // calculate the differences between the start and end X/Y positions for each of our points
+        let delta1x = end1.x - start1.x
+        let delta1y = end1.y - start1.y
+        let delta2x = end2.x - start2.x
+        let delta2y = end2.y - start2.y
+
+        // create a 2D matrix from our vectors and calculate the determinant
+        let determinant = delta1x * delta2y - delta2x * delta1y
+
+        if abs(determinant) < 0.0001 {
+            // if the determinant is effectively zero then the lines are parallel/colinear
+            return nil
+        }
+
+        // if the coefficients both lie between 0 and 1 then we have an intersection
+        let ab = ((start1.y - start2.y) * delta2x - (start1.x - start2.x) * delta2y) / determinant
+
+        if ab > 0 && ab < 1 {
+            let cd = ((start1.y - start2.y) * delta1x - (start1.x - start2.x) * delta1y) / determinant
+
+            if cd > 0 && cd < 1 {
+                // lines cross – figure out exactly where and return it
+                let intersectX = start1.x + ab * delta1x
+                let intersectY = start1.y + ab * delta1y
+                return (intersectX, intersectY)
+            }
+        }
+
+        // lines don't cross
+        return nil
     }
 
 
