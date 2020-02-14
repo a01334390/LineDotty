7e15329ce3bcba65108f22c43c6ca1134932b0eb
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-13 20:01:23 -0600

    Added levelUp method

--- LineDotty/LineDotty/ViewController.swift
+++ LineDotty/LineDotty/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentLevel = 0 // Current Level to display
+    var connections = [UIView]() // Connection dots to display
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        levelUp()
+    }
+    
+    /**
+        Creates a new and harder level
+     - Returns:
+            - Void
+        
+     */
+    func levelUp() {
+        currentLevel += 1
+        // Restore connections to original status
+        connections.forEach { $0.removeFromSuperview() }
+        // Remove all connections
+        connections.removeAll()
+        // Add five draggable areas every time
+        for _ in 1...(currentLevel + 4) {
+            let connection = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 44, height: 44)))
+            connection.backgroundColor = .white
+            connection.layer.cornerRadius = 22
+            connection.layer.borderWidth = 2
+            connections.append(connection)
+            view.addSubview(connection)
+        }
     }
 
 
